dfb11ab0c610219e6441a88f853fbbb8df79db7a
Faisal <Faisal_jobb@outlook.com>
2023-07-12 17:04:57 +0200

    added Registration text inside a <a> tag for switcing site

--- pizza_shop_api/src/main/java/com/example/pizza_shop_api/PizzaShopApiApplication.java
+++ pizza_shop_api/src/main/java/com/example/pizza_shop_api/PizzaShopApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PizzaShopApiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaShopApiApplication.class, args);
 	}
